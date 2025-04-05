b0c885642af6cda8fd52ccf3a14275865664363e
Vllad Gordeev <vlladg@gmail.com>
2025-04-05 23:57:10 +0300

    Добавлена кнопка Reset для сброса счётчика

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement } from './counterSlice';
+import { increment, decrement, reset } from './counterSlice';
 
 const Counter = () => {
     const count = useSelector((state) => state.counter.value);
@@ -11,6 +11,7 @@ const Counter = () => {
             <h1>Counter: {count}</h1>
             <button onClick={() => dispatch(increment())}>+</button>
             <button onClick={() => dispatch(decrement())}>-</button>
+            <button onClick={() => dispatch(reset())}>Reset</button>
         </div>
     );
 };
