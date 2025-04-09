5208241a73f472e28f8a8ecfcfbae1265d504061
Vllad Gordeev <vlladg@gmail.com>
2025-04-10 01:22:54 +0300

    Загадка для Макса

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from 'react';
 import Counter from './features/counter/Counter';
 
 function App() {
-  return (
-      <div className="App">
-        <Counter />
-      </div>
-  );
+    return (
+        <div className="App">
+            <Counter/>
+            <Counter/>
+        </div>
+    );
 }
 
 export default App;
